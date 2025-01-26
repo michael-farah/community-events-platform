fda2bad49ab94c7de71a7842c467a7ea9854d7d7
michael-farah <michaelfarah@live.co.uk>
2025-01-26 14:01:22 +0700

    feat: allows users with sufficient permissions to create and publish events

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { Layout } from "./components/Layout";
 import { EventsPage } from "./pages/EventsPage";
 import { HomePage } from "./pages/HomePage";
 import { EventPage } from "./pages/EventPage";
+import { CreateEventPage } from "./pages/CreateEventPage";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
       { path: "/", element: <HomePage /> },
       { path: "events", element: <EventsPage /> },
       { path: "events/:eventId", element: <EventPage /> },
+      { path: "events/create", element: <CreateEventPage /> },
     ],
   },
 ]);
